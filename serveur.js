90cbec18b8ea453b0673fed096fb540706bd466e
MathieuSchetagne <mathieu1166@me.com>
2018-02-02 15:50:32 -0500

    création du tableau

--- serveur.js
+++ serveur.js
@@ -1,17 +1,38 @@
 
 const http = require("http");
+const fs = require("fs");
+let provinces;
+
+ fs.readFile('provinces.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    provinces = JSON.parse(data);
+   });
+
 http.createServer((request,response) => 
 {
     console.log('branchement sur le port 8888')
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World"); 
+
+    let html = "<html>";
+    html += "<table>";
+    for(let nom in provinces )   {
+        html += "<tr>";
+        html += "<td>";
+        html += nom;
+        html += "</td>";
+        html += "<td>";
+        html += provinces[nom];
+        html += "</td>";
+        html += "</tr>";
+    }
+   
+   
+    html += "</table>";
+    html += "</html>";
+
+    console.log(provinces);
+
+    response.writeHead(200, {"Content-Type": "text/html; charset = UTF-8"});
+    response.write(html); 
     response.end(); 
-
-    fs.readFile('provinces.JSON', 'utf8', function (err, data) {
-        if (err) throw err;
-        obj = JSON.parse(data);
-       });
-
-    console.log(obj);
 
 }).listen(8888);
