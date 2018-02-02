bc93c790ec2291ec7e6993029a3c0929e75e17da
MathieuSchetagne <mathieu1166@me.com>
2018-02-02 14:21:41 -0500

    test4.final

--- exercice3.js
+++ exercice3.js
@@ -1,21 +1,20 @@
 "use strict";
 /* importation du module «fs» (file system) */
-var fs = require("fs");
+const fs = require("fs");
 
 /* lecture synchrone, la fonction: fs.readFileSync() 
 bloque l'éxécution des instructions suivantes */
 
-var data = fs.readFileSync('03_fichier.txt');
+let data = fs.readFileSync('fichierText.txt');
 
 console.log(data.toString());
 console.log("Fin du programme");
 
 console.log("////////////////////////////////////////////");
 
-"use strict";
-var fs = require("fs");
+
 /* La fonction fs.readFile() est asynchrone elle ne bloque pas l'éxécution des instructions suivantes */
-fs.readFile('03_fichier.txt', (err, data) => {
+fs.readFile('fichierText.txt', (err, data) => {
  if (err) return console.error(err);
  console.log(data.toString());
 });
